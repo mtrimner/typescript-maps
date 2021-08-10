c9b29d464b2e0dee8de33c1e7d40e131fb538a2b
mtrimner <matt.trimner@gmail.com>
2021-08-10 13:40:46 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { CustomMap } from './CustomMap';
+import { User } from './User';
+// import { Company } from './Company';
+
+const user = new User();
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
